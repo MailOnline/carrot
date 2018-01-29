465a68241925626d3fbaf1b1b3a13f853d0089c0
Matt Felstead <matt.felstead@mailonline.co.uk>
2018-01-29 16:20:15 +0000

    feat: updated logger to log map to provided logging handler

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject carrot "2.0.0"
+(defproject carrot "2.0.1-SNAPSHOT"
   :description "A Clojure library designed to providing the implementation of  RabbitMq delayed retry mechanism."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
--- src/carrot/exp_backoff.clj
+++ src/carrot/exp_backoff.clj
@@ -5,7 +5,6 @@
             [langohr.exchange :as le]
             [langohr.consumers :as lc]))
 
-
 (defn next-ttl [retry-attempts retry-config]
   (int (min (* (.pow (BigInteger. "2") (int retry-attempts)) (:initial-ttl retry-config)) (:max-ttl retry-config))))
 
@@ -15,8 +14,13 @@
         exchange (if (> max-retry retry-attempts) retry-exchange dead-letter-exchange)
         calculated-ttl (apply (-> retry-config :next-ttl-function) [retry-attempts retry-config])
         exp-retry-queue-name (str routing-key "."  (str calculated-ttl) ".retry" )]
-    (when logger-fn (logger-fn "LOGME ns=carrot.core name=current-retry-attempt message:" (:message-id meta) retry-attempts))
-    (when logger-fn (logger-fn "Sending message " (:message-id meta)  " to the exchange: " exchange))
+    (when logger-fn
+          (logger-fn {:type :metric
+                      :log-ns "carrot.core"
+                      :txid (:message-id meta)
+                      :name "nack"
+                      :params {:retry-attempts retry-attempts
+                               :exchange exchange}}))
     (lq/declare ch exp-retry-queue-name
                 {:durable true
                  :auto-delete true
@@ -35,7 +39,6 @@
                  {:persistent true
                   :headers {"retry-attempts" (inc retry-attempts)}})))
   (lb/ack ch (:delivery-tag meta)))
-
 
 (defn declare-system [channel
                       {:keys [retry-exchange dead-letter-exchange message-exchange exchange-type exchange-config retry-queue-config]
